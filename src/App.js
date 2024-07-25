5245952d43877981a4435f7cd042b0b78dbedef7
SJVenable <sjvenable74@gmail.com>
2024-07-25 22:34:31 +0100

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import styles from './App.module.css';
+import SlideshowTitle from './SlideshowTitle';
+
+function App() {
+  return (
+    <div className="App">
+      <SlideshowTitle></SlideshowTitle>
+    </div>
+  );
+}
+
+export default App;
